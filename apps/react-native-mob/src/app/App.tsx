664da5d7122b40c33da069ddc3b4884ece356863
Yarovenko Dmitry <yarovenkodmv@gmail.com>
2022-04-18 10:50:57 +0300

    feat:add starter project with NX monorepository

---
+++ apps/react-native-mob/src/app/App.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+export const App = () => {
+
+  return (
+    <>
+      <Text style={[styles.textXL, styles.appTitleText]}>
+        Welcome ReactNativeMob 👋
+      </Text>
+      <Text style={[styles.textLg]} >
+        TEST 👋
+      </Text>
+    </>
+  );
+};
+const styles = StyleSheet.create({
+  textLg: {
+    fontSize: 24,
+  },
+  textXL: {
+    fontSize: 48,
+  },
+  appTitleText: {
+    paddingTop: 12,
+    fontWeight: '500',
+  },
+});
+
+export default App;
