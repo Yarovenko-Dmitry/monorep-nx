6d92a3b86ed901a6683095bb25cf0b9dc5c546f7
Yarovenko Dmitry <yarovenkodmv@gmail.com>
2022-04-22 11:09:23 +0300

    feat: add additional react-web-add-test app

---
+++ apps/react-web-add-test/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'react-web-add-test',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/react-web-add-test',
+};
